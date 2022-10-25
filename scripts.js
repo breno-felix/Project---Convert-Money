899afb91065356894d0d7c786432bdb1a54e6ef2
breno-felix <brenodev.felix@gmail.com>
2022-10-25 08:28:38 -0300

    Primeiro commit - Apliquei a função CSS Flexbox

---
+++ scripts.js
@@ -0,0 +1,63 @@
+const button = document.getElementById("convert-button");
+const select = document.getElementById("currency-select");
+
+const convertValues = async () => {
+  const inputReais = document.getElementById("input-real").value;
+  const realValueText = document.getElementById("real-value-text");
+  const currencyValueText = document.getElementById("currency-value-text");
+
+  const data = await fetch(
+    "https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL"
+  ).then((response) => response.json());
+
+  const dolar = data.USDBRL.high;
+  const euro = data.EURBRL.high;
+  const bitcoin = data.BTCBRL.high;
+
+  realValueText.innerHTML = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(inputReais);
+
+  if (select.value === "US$ Dólar americano") {
+    currencyValueText.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(inputReais / dolar);
+  }
+
+  if (select.value === "€ Euro") {
+    currencyValueText.innerHTML = new Intl.NumberFormat("de-DE", {
+      style: "currency",
+      currency: "EUR",
+    }).format(inputReais / euro);
+  }
+
+  if (select.value === "Bitcoin") {
+    currencyValueText.innerHTML = inputReais / (bitcoin * 1000); // multipliquei por mil pois a API estava enviendo 102.228 e o programa tava lendo como 102,228
+  }
+
+  changeCurrency();
+};
+
+changeCurrency = () => {
+  const currencyName = document.getElementById("currency-name");
+  const currencyImg = document.getElementById("currency-img");
+
+  if (select.value === "€ Euro") {
+    currencyName.innerHTML = "Euro";
+    currencyImg.src = "./assets/euro.png";
+  }
+
+  if (select.value === "US$ Dólar americano") {
+    currencyName.innerHTML = "Dólar Americano";
+    currencyImg.src = "./assets/eua.png";
+  }
+
+  if (select.value === "Bitcoin") {
+    currencyName.innerHTML = "Bitcoin";
+    currencyImg.src = "./assets/bitcoin.png";
+  }
+};
+
+button.addEventListener("click", convertValues);
